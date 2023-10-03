8ec1a90d24937b155f4b2754fc6ddda50bceae6e
Itai Levi <italev46@gmail.com>
2023-10-03 17:06:12 -0400

    feat(org): add create_org_saml_connection_link request

--- src/apis/org_service_api.rs
+++ src/apis/org_service_api.rs
@@ -9,9 +9,11 @@
  */
 
 
+use std::collections::HashMap;
+
 use reqwest;
 
-use crate::apis::ResponseContent;
+use crate::{apis::ResponseContent, models::create_org_saml_connection_link_response};
 use crate::models::FetchOrgOrderBy;
 use super::{Error, configuration};
 
@@ -43,6 +45,13 @@ pub struct CreateOrgParams {
 #[derive(Clone, Debug, Default)]
 pub struct DisallowSamlParams {
     pub org_id: String
+}
+
+/// struct for passing parameters to the method [`create_org_saml_connection_link`]
+#[derive(Clone, Debug, Default)]
+pub struct CreateOrgSamlConnectionLinkParams {
+    pub org_id: String,
+    pub expires_in_seconds: Option<i64>
 }
 
 /// struct for passing parameters to the method [`fetch_org`]
@@ -125,6 +134,15 @@ pub enum CreateOrgError {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum DisallowSamlError {
+    Status401(serde_json::Value),
+    Status404(serde_json::Value),
+    UnknownValue(serde_json::Value),
+}
+
+/// struct for typed errors of method [`create_org_saml_connection_link`]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum CreateOrgSamlConnectionLinkError {
     Status401(serde_json::Value),
     Status404(serde_json::Value),
     UnknownValue(serde_json::Value),
@@ -349,6 +367,49 @@ pub async fn disallow_saml(configuration: &configuration::Configuration, params:
         Err(Error::ResponseError(local_var_error))
     }
 }
+
+pub async fn create_org_saml_connection_link(configuration: &configuration::Configuration, params: CreateOrgSamlConnectionLinkParams) -> Result<crate::models::CreateOrgSamlConnectionLinkResponse, Error<CreateOrgSamlConnectionLinkError>> {
+    let local_var_configuration = configuration;
+
+    // unbox the parameters
+    let org_id = params.org_id;
+    let create_org_saml_connection_link_body = {
+        let mut request_body = HashMap::new();
+        if let Some(param_value) = params.expires_in_seconds {
+            request_body.insert("expires_in_seconds".to_string(), param_value.to_string());
+        }
+        request_body
+    };
+
+    let local_var_client = &local_var_configuration.client;
+
+    let local_var_uri_str = format!("{}/api/backend/v1/org/{org_id}/create_saml_connection_link", local_var_configuration.base_path, org_id=crate::apis::urlencode(org_id));
+    let mut local_var_req_builder = local_var_client.request(reqwest::Method::POST, local_var_uri_str.as_str());
+    local_var_req_builder = local_var_req_builder.json(&create_org_saml_connection_link_body);
+
+    if let Some(ref local_var_user_agent) = local_var_configuration.user_agent {
+        local_var_req_builder = local_var_req_builder.header(reqwest::header::USER_AGENT, local_var_user_agent.clone());
+    }
+    if let Some(ref local_var_token) = local_var_configuration.bearer_access_token {
+        local_var_req_builder = local_var_req_builder.bearer_auth(local_var_token.to_owned());
+    };
+
+
+    let local_var_req = local_var_req_builder.build()?;
+    let local_var_resp = local_var_client.execute(local_var_req).await?;
+
+    let local_var_status = local_var_resp.status();
+    let local_var_content = local_var_resp.text().await?;
+
+    if !local_var_status.is_client_error() && !local_var_status.is_server_error() {
+        serde_json::from_str(&local_var_content).map_err(Error::from)
+    } else {
+        let local_var_entity: Option<CreateOrgSamlConnectionLinkError> = serde_json::from_str(&local_var_content).ok();
+        let local_var_error = ResponseContent { status: local_var_status, content: local_var_content, entity: local_var_entity };
+        Err(Error::ResponseError(local_var_error))
+    }
+}
+
 
 pub async fn fetch_org(configuration: &configuration::Configuration, params: FetchOrgParams) -> Result<crate::models::FetchOrgResponse, Error<FetchOrgError>> {
     let local_var_configuration = configuration;
