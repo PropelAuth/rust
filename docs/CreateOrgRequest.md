4cfd0c8cd693ceec19ab634ea4d3558aaf93722d
Itai Levi <itai@propelauth.com>
2023-10-05 11:54:03 -0400

    [TIC-363] Add missing fields to `create_org` (#14)
    * feat(org): add arguments to update_org_request (#16)
    
    * feat(org): add arguments to create_org request
    
    * docs(create_org): add missing fields to docs

--- docs/CreateOrgRequest.md
+++ docs/CreateOrgRequest.md
@@ -2,10 +2,12 @@
 
 ## Properties
 
-Name | Type | Description | Notes
------------- | ------------- | ------------- | -------------
-**name** | **String** |  | 
+| Name                                  | Type               | Description | Notes |
+| ------------------------------------- | ------------------ | ----------- | ----- |
+| **name**                              | **String**         |             |
+| **domain**                            | **Option<String>** |             |
+| **enable_auto_joining_by_domain**     | **Option<bool>**   |             |
+| **members_must_have_matching_domain** | **Option<bool>**   |             |
+| **max_users**                         | **Option<i32>**    |             |
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
-
-
--- src/models/create_org_request.rs
+++ src/models/create_org_request.rs
@@ -15,12 +15,24 @@
 pub struct CreateOrgRequest {
     #[serde(rename = "name")]
     pub name: String,
+    #[serde(rename = "domain", skip_serializing_if = "Option::is_none")]
+    pub domain: Option<String>,
+    #[serde(rename = "enable_auto_joining_by_domain", skip_serializing_if = "Option::is_none")]
+    pub enable_auto_joining_by_domain: Option<bool>,
+    #[serde(rename = "members_must_have_matching_domain", skip_serializing_if = "Option::is_none")]
+    pub members_must_have_matching_domain: Option<bool>,
+    #[serde(rename = "max_users", skip_serializing_if = "Option::is_none")]
+    pub max_users: Option<i32>,
 }
 
 impl CreateOrgRequest {
     pub fn new(name: String) -> CreateOrgRequest {
         CreateOrgRequest {
             name,
+            domain: None,
+            enable_auto_joining_by_domain: None,
+            members_must_have_matching_domain: None,
+            max_users: None,
         }
     }
 }
