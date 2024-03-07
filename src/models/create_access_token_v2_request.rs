a951f3ed201ccc324c23b73a1729e165fd633c2a
Itai Levi <italev46@gmail.com>
2024-03-07 14:07:19 -0500

    feat(create_access_token_v2_request): create new request to not break backwards compatability

---
+++ src/models/create_access_token_v2_request.rs
@@ -0,0 +1,28 @@
+#[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
+
+pub struct CreateAccessTokenV2Request {
+    #[serde(rename = "user_id")]
+    pub user_id: String,
+    #[serde(rename = "duration_in_minutes")]
+    pub duration_in_minutes: u64,
+
+    #[serde(
+        rename = "with_active_org_support",
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub with_active_org_support: Option<bool>,
+
+    #[serde(rename = "active_org_id", skip_serializing_if = "Option::is_none")]
+    pub active_org_id: Option<String>,
+}
+
+impl CreateAccessTokenV2Request {
+    pub fn new(user_id: String, duration_in_minutes: u64) -> CreateAccessTokenV2Request {
+        CreateAccessTokenV2Request {
+            user_id,
+            duration_in_minutes,
+            active_org_id: None,
+            with_active_org_support: None,
+        }
+    }
+}
