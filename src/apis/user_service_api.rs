1715ab04d477f7b41dd11ff0acd2792f596ea55b
Itai Levi <italev46@gmail.com>
2024-06-14 11:14:07 -0400

    feat(user): add `logout_all_user_sessions` api request

--- src/apis/user_service_api.rs
+++ src/apis/user_service_api.rs
@@ -171,6 +171,15 @@ pub enum DisableUserError {
     UnknownValue(serde_json::Value),
 }
 
+/// struct for typed errors of method [`logout_all_user_sessions`]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum LogoutAllUserSessionsError {
+    Status401(serde_json::Value),
+    Status404(serde_json::Value),
+    UnknownValue(serde_json::Value),
+}
+
 // struct for typed errors of method [`resend_email_confirmation`]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
@@ -543,6 +552,50 @@ pub async fn resend_email_confirmation(
         serde_json::from_str(&local_var_content).map_err(Error::from)
     } else {
         let local_var_entity: Option<ResendEmailConfirmationError> =
+            serde_json::from_str(&local_var_content).ok();
+        let local_var_error = ResponseContent {
+            status: local_var_status,
+            content: local_var_content,
+            entity: local_var_entity,
+        };
+        Err(Error::ResponseError(local_var_error))
+    }
+}
+
+pub async fn logout_all_user_sessions(
+    configuration: &configuration::Configuration,
+    user_id: String,
+) -> Result<crate::models::SuccessfulResponse, Error<LogoutAllUserSessionsError>> {
+    let local_var_configuration = configuration;
+
+    let local_var_client = &local_var_configuration.client;
+
+    let local_var_uri_str = format!(
+        "{}/api/backend/v1/user/{user_id}/logout_all_sessions",
+        local_var_configuration.base_path,
+        user_id = crate::apis::urlencode(user_id)
+    );
+    let mut local_var_req_builder =
+        local_var_client.request(reqwest::Method::POST, local_var_uri_str.as_str());
+
+    if let Some(ref local_var_user_agent) = local_var_configuration.user_agent {
+        local_var_req_builder =
+            local_var_req_builder.header(reqwest::header::USER_AGENT, local_var_user_agent.clone());
+    }
+    if let Some(ref local_var_token) = local_var_configuration.bearer_access_token {
+        local_var_req_builder = local_var_req_builder.bearer_auth(local_var_token.to_owned());
+    };
+
+    let local_var_req = local_var_req_builder.build()?;
+    let local_var_resp = local_var_client.execute(local_var_req).await?;
+
+    let local_var_status = local_var_resp.status();
+    let local_var_content = local_var_resp.text().await?;
+
+    if !local_var_status.is_client_error() && !local_var_status.is_server_error() {
+        serde_json::from_str(&local_var_content).map_err(Error::from)
+    } else {
+        let local_var_entity: Option<LogoutAllUserSessionsError> =
             serde_json::from_str(&local_var_content).ok();
         let local_var_error = ResponseContent {
             status: local_var_status,
